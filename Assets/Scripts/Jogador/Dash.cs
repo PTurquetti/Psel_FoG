0ecf1eef82dd5fa231ccb2b8287da5677df77633
PTurquetti <pauloturquetti29@gmail.com>
2024-05-16 15:23:21 -0300

    Update dash

--- Assets/Scripts/Jogador/Dash.cs
+++ Assets/Scripts/Jogador/Dash.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Rigidbody2D Rigidbody;
+
 
     // Update is called once per frame
     void Update()
