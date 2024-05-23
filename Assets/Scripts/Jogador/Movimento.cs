0f51a737d7436a20903ee8c0cb50a33ba3288225
PTurquetti <pauloturquetti29@gmail.com>
2024-05-23 15:48:47 -0300

    wallslide primeira versao

--- Assets/Scripts/Jogador/Movimento.cs
+++ Assets/Scripts/Jogador/Movimento.cs
@@ -11,15 +11,54 @@ public class Movimento : MonoBehaviour
     [SerializeField] private float MultiplicadorVelocidade;
     [SerializeField] private Transform PeDoPersonagem;
     [SerializeField] private LayerMask Chao;
+    private bool PertoDoChao;
     
-    
+
+
     //O corpo do jogador
     [SerializeField] private Rigidbody2D Corpo;
     //Para ele não pular infinitamente
     private bool PodePular = false;
     [SerializeField] private float ForcaPulo;
 
+    [SerializeField] private LayerMask Parede;
+    [SerializeField] private Transform WallCheck;
+    [SerializeField] private float VelocidadeWallSliding;
+    private bool TocandoParede;
+    private bool Wallsliding;
+    
+
+
+
     void Update()
+    {
+        Andar();
+        Pular();
+        WallSlide();
+        
+    }
+
+    public void WallSlide()
+    {
+        TocandoParede = Physics2D.OverlapBox(WallCheck.position, new Vector2(.2f, .8f), 0, Parede);
+
+        if (TocandoParede && !PertoDoChao)
+        {
+            Wallsliding = true;
+        }
+        else
+        {
+            Wallsliding = false;
+        }
+
+        if (Wallsliding)
+        {
+            Corpo.velocity = new Vector2(Corpo.velocity.x, Mathf.Clamp(Corpo.velocity.y, -VelocidadeWallSliding, float.MaxValue));
+        }
+    
+    }
+
+    public void Andar()
     {
         //Define a velocidade do corpo baseada na tecla pressionada (Input.GetAxisRaw("Horizontal"))
         //A função retorna 1 se a seta pra direita ou D foram pressionados
@@ -27,6 +66,7 @@ public class Movimento : MonoBehaviour
         //Retorna 0 se nenhum direcional foi pressionado
         float movimento_horizontal = Velocidade * Input.GetAxisRaw("Horizontal");
 
+        // Correr rapido com LShift
         if (Input.GetKey(KeyCode.LeftShift))
         {
             movimento_horizontal *= MultiplicadorVelocidade;
@@ -35,14 +75,17 @@ public class Movimento : MonoBehaviour
 
         //Neste caso, não se usa Time.deltaTime, porque RigidBody2D.velocity já opera baseado na taxa de frames
         Corpo.velocity = new Vector2(movimento_horizontal, Corpo.velocity.y);
+    }
 
+    public void Pular()
+    {
         //Cria uma caixa, se a caixa colidir com o chao, pode pular
         //Nessa função se passa a posição, tamanho, angulo e distancia(tamanho) em relação a direção
         //Tambem passa um layer mask, pra que somente os layers associados a Chao sejam considerados
-        bool PertoDoChao = Physics2D.BoxCast(PeDoPersonagem.position, new Vector2(0.5f, 0.2f), 0f, Vector2.down, 0.1f, Chao);
-                
+        PertoDoChao = Physics2D.BoxCast(PeDoPersonagem.position, new Vector2(0.5f, 0.2f), 0f, Vector2.down, 0.1f, Chao);
+
         //Se o acerto tem um resultado não nulo, pode pular
-        if(PertoDoChao)
+        if (PertoDoChao)
         {
             PodePular = true;
         }
